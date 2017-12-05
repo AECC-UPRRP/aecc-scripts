944b09055cd05207f132175f14c1d4d178456948
asvnpr <vega360@gmail.com>
2017-12-04 21:16:42 -0400

    updated script to only overwrite the users file when there's a change and to actually restart the freeradius so the changes take place

--- update-wifi-users.py
+++ update-wifi-users.py
@@ -2,29 +2,32 @@
 
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+import hashlib
+import subprocess
 
 # google api credentials for google drive API
 scope = ['https://spreadsheets.google.com/feeds']
-creds = ServiceAccountCredentials.from_json_keyfile_name('/home/aecc-directiva/wifi_manage/aecc-uprrp-google-api.json', scope)
+cred_file = '/home/aecc-directiva/wifi_manage/aecc-uprrp-google-api.json'
+creds = ServiceAccountCredentials.from_json_keyfile_name(cred_file, scope)
 client = gspread.authorize(creds)
 
 # Find a workbook by name and open the first sheet
 # Make sure you use the right name here.
 sheet = client.open("Formulario para Ingresar a AECC (Responses)").sheet1
 
-# acquire student numbers 
+# acquire student numbers
 sids = sheet.col_values(6)
 
 # acquire phone numbers
 phones = sheet.col_values(7)
 
-# bool confirming members have payed 
+# bool confirming members have payed
 confirmation = sheet.col_values(19)
 
-# freeradius entries
+# freeradius users entries
 entries = []
 
-# parse all entries and only add them if it's a confirmed member 
+# parse all entries and only add them if it's a confirmed member
 for i in range(1,len(sids)):
 	if (confirmation[i] == '1'):
 		user = sids[i]
@@ -33,10 +36,27 @@ for i in range(1,len(sids)):
 		#print(entry)
 		entries.append(entry)
 
-with open('/etc/freeradius/users.template', 'r') as f: content = f.readlines()
+template = '/etc/freeradius/users.template'
+users = '/etc/freeradius/users'
+
+with open(template, 'r') as f: content = f.readlines()
+with open(users, 'r') as u: curr = u.read()
 
 for e in entries:
 	content.append(e+"\n")
 c = ''.join(content)
 
-with open('/etc/freeradius/users', 'w') as f: f.write(c)
+f.close()
+
+# calculate md5 sums of current users file and potential new one
+curr_hash = hashlib.md5(curr.encode('utf-8')).hexdigest()
+c_hash = hashlib.md5(c.encode('utf-8')).hexdigest()
+
+u.close()
+
+# if our created new file is different from our current one,
+# then overwrite the old one and restart the freeradius service
+if(c_hash != curr_hash):
+    with open(users, 'w') as f: f.write(c)
+    f.close()
+    subprocess.run(['service', 'freeradius', 'restart'])
